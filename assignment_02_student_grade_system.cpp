5ba05899421e0465bce42a35666de1d7055ed4a9
GID-FRIK <atdoku003@st.ug.edu.gh>
2026-08-02 18:58:12 +0000

    completed assignment
    completed Assignment1

--- assignment_02_student_grade_system.cpp
+++ assignment_02_student_grade_system.cpp
@@ -44,5 +44,21 @@
 // =============================================================================
 
 #include <iostream>
-using namespace std;
+using nespace std;
+int getGrade(int score) {
+    if (score < 0 || score > 100) {
+        return '\0'; // Invalid score
+    } else if (score >= 80) {
+        return 'A';
+    } else if (score >= 70) {
+        return 'B';
+    } else if (score >= 60) {
+        return 'C';
+    } else if (score >= 50) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+
 
